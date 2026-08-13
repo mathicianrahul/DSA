fad9c7e4c998e2514553e268b06b29609f89adea
Rahul Girase <rgirase313@gmail.com>
2026-08-13 18:48:00 +0530

    Add Day 1

---
+++ DAY-1/variable.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   cout<<"rahhul" <<endl <<"Girase";
+//   return 0 ; 
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   char grade = 'a' ; // 97
+//   int num = grade;
+//   cout<< num <<endl;
+//   return 0;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   double price = 100.99;
+  
+//   cout<<int(price) <<endl;
+//   return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+int main(){
+  int age;
+  cout<<"Enter the Age:";
+  cin>>age;
+  cout<< "your age is:"<<age<<endl;
+  return 0;
+}
