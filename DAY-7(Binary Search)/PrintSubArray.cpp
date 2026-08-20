9b94109dcbc4308793cf1076e1efd0eb3a9ba27a
mathicianrahul <rgirase312@gmail.com>
2026-08-20 23:34:12 +0530

    Solved PrintSubArrays Problem Self

---
+++ DAY-7(Binary Search)/PrintSubArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printSubarrays(int *arr, int n) {
+  for (int start = 0; start < n; start++) {
+    for (int end = start; end < n; end++) {
+      for (int i = start; i <= end; i++) {
+        cout << arr[i];
+      }
+      if (end < n - 1) {
+        cout << ", ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int n = 5;
+
+  printSubarrays(arr, n);
+
+  return 0;
+}
+
