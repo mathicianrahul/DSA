2b7610afdef996f4b72e4c4cf765fc06d1be51c8
mathicianrahul <rgirase312@gmail.com>
2026-09-10 20:22:40 +0530

    restart DSA

--- DAY-6(ARRAYS)/LinearSearch.cpp
+++ DAY-6(ARRAYS)/LinearSearch.cpp
@@ -1,23 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n, int key){
-    for(int i=0; i<n; i++){
-
-        if(arr[i]==key){
-            cout<<"Element Found At Index: ";
-            return i;
-        }
+int linearSearch(int *arr, int n, int key) {
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == key) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
+int main() {
 
-int main(){
-    int arr[]={2,1,52,1,45,56,25,2,32,65,59,45,85};
-    int n=sizeof(arr)/sizeof(int);
-
-    cout<<linearSearch(arr, n, 59)<<endl;
-    return 0;
-
-}//changed
+  int arr[] = {12, 54, 64, 65, 73, 23, 54, 67, 86, 73, 56};
+  int n = sizeof(arr) / sizeof(int);
+  int a = linearSearch(arr, n, 124);
+  cout << a << endl;
+  return 0;
+}
