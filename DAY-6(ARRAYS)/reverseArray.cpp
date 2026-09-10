c06a17a19f665eb6a34a862f32f7c154d1417d04
mathicianrahul <rgirase312@gmail.com>
2026-09-10 21:01:11 +0530

    array reverse

--- DAY-6(ARRAYS)/reverseArray.cpp
+++ DAY-6(ARRAYS)/reverseArray.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void printArr(int *arr, int n) {
+void reversArray(int *arr, int n) {
+  int coppyArr[n];
+
   for (int i = 0; i < n; i++) {
-    cout << arr[i] << ",";
+    int j = n - i - 1;
+    coppyArr[i] = arr[j];
+  }
+
+  for (int i = 0; i < n; i++) {
+    arr[i] = coppyArr[i];
+    cout << arr[i] << ',';
   }
 }
 
 int main() {
-
-  int arr[] = {5, 4, 3, 2, 1};
+  int arr[] = {1, 2, 3, 4, 5};
   int n = sizeof(arr) / sizeof(int);
 
-  int copyArr[n];
+  reversArray(arr, n);
 
-  for (int i = 0; i < n; i++) {
-    int j = n - i - 1;
-    copyArr[i] = arr[j];
-  }
-
-  for (int i = 0; i < n; i++) {
-    arr[i] = copyArr[i];
-  }
-
-  printArr(arr, n);
   return 0;
 }
-// check files
