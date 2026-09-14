3b3d5f1f5da72a9a262a9d26701cfa24dc5791db
mathicianrahul <rgirase312@gmail.com>
2026-09-14 11:43:50 +0530

    Learn Insertion Sort

---
+++ DAY-14/insertionSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+void printArr(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << ' ';
+  }
+  cout << endl;
+}
+
+void insertionSort(int *arr, int n) {
+  for (int i = 1; i < n; i++) {
+    int curr = arr[i];
+    int prev = i - 1;
+
+    while (prev >= 0 && arr[prev] > curr) {
+      swap(arr[prev], arr[prev + 1]);
+      prev--;
+    }
+  }
+  printArr(arr, n);
+}
+
+int main() {
+  int arr[] = {5, 4, 1, 3, 2};
+  int n = sizeof(arr) / sizeof(int);
+  insertionSort(arr, n);
+  return 0;
+}
